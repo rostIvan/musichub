48ff8e03140680c9707288d7fcf618ee69614fc3
rostIvan <bbwgd77@gmail.com>
2019-05-16 15:02:32 +0300

    Add `Lesson` model

--- lessons/models.py
+++ lessons/models.py
@@ -1,3 +1,18 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+__all__ = ['Lesson']
+
+User = get_user_model()
+
+
+class Lesson(models.Model):
+    user = models.ForeignKey(User, related_name='lessons',
+                             on_delete=models.SET_NULL, null=True, blank=False)
+    text = models.TextField(blank=False, null=False)
+
+    def __str__(self):
+        return f'{self.id}, {self.user.name} => {self.text[:20]}'
+
+    class Meta:
+        db_table = 'lessons'
