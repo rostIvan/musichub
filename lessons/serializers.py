2fc7a64665e8c116c013dcb0d9135597feb531b0
rostIvan <bbwgd77@gmail.com>
2019-05-20 22:01:13 +0300

    Add `likes_link` field to `LessonSerializer` class

--- lessons/serializers.py
+++ lessons/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from rest_framework.reverse import reverse
 
 from lessons.models import Lesson, Like
 from users.serializers import UserSerializer
@@ -6,10 +7,16 @@ from users.serializers import UserSerializer
 
 class LessonSerializer(serializers.ModelSerializer):
     user = UserSerializer()
+    likes_link = serializers.SerializerMethodField()
+
+    def get_likes_link(self, instance):
+        request = self.context['request']
+        sub_link = reverse('lesson_likes', args=(instance.id,))
+        return request.build_absolute_uri(sub_link)
 
     class Meta:
         model = Lesson
-        fields = ('id', 'title', 'text', 'created', 'user')
+        fields = ('id', 'title', 'text', 'created', 'user', 'likes_link')
         read_only_fields = ('id', 'created', 'user')
 
 
