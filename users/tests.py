b9f0d34a96e90ab4cd31424f6f2a3efbd9699c58
rostIvan <bbwgd77@gmail.com>
2019-05-21 15:27:27 +0300

    Add `users` simple smoke tests, add `mixer` to `requirements.txt`

--- users/tests.py
+++ users/tests.py
@@ -1,1 +1,16 @@
-# Create your tests here.
+from mixer.backend.django import mixer
+from rest_framework.reverse import reverse
+from rest_framework.test import APITestCase
+
+from users.models import User
+
+
+class UserSmokeTest(APITestCase):
+    def test_user_empty_list(self):
+        response = self.client.get(reverse('users-list'))
+        self.assertIs(len(response.data), 0)
+
+    def test_user_list(self):
+        mixer.cycle(17).blend(User)
+        response = self.client.get(reverse('users-list'))
+        self.assertIs(len(response.data), 17)
